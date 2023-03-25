dfabb5caed379c87faee51e7b45b1c48d54a371c
Shriamrut V <vshriamrut@vmware.com>
2023-03-25 20:44:08 +0530

    Add todays work for transformer

--- m-gpt/bigram.py
+++ m-gpt/bigram.py
@@ -5,11 +5,12 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size  = 32
 block_size = 8
-max_iters = 3000
+max_iters = 5000
 eval_interval = 300
-learning_rate = 1e-2
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
+n_embd = 32
 #---------------------------------------
 
 print(f"Device {device}")
@@ -56,14 +57,54 @@ def estimate_loss():
         out[split] = losses.mean()
     model.train()
     return out
+
+class MuliHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim = -1)
+       
+class Head(nn.Module):
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias = False)
+        self.value = nn.Linear(n_embd, head_size, bias = False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+    
+    def forward(self, x):
+        B,T,C = x.shape
+
+        # compute attention score
+        k = self.key(x)
+        q = self.query(x)
+        wei = q @ k.transpose(-2, -1) * C**0.5 # (B,T,C) @ (B, C, T) = (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf")) # (B, T, T)
+        wei = F.softmax(wei, dim = -1) # (B, T, T)
+        
+        # perform weighted aggregation of values
+        v = self.value(x) # (B, T, T)
+        out = wei @ v # (B, T, T) @ (B, T, T) = (B, T, T)
+        return out
     
 class BigramLanguageModel(nn.Module):
-    def __init__(self, vocab_size):
+    def __init__(self):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.sa_heads = MuliHeadAttention(4, n_embd//4) # 4 heads of 8 dimensional self attention
+        self.lm_head = nn.Linear(n_embd, vocab_size)
         
     def forward(self, idx, targets = None):
-        logits = self.token_embedding_table(idx) # (B,T,C)
+        B, T = idx.shape
+        tok_emb = self.token_embedding_table(idx) # (B,T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device = device)) # (T,C)
+        x = tok_emb + pos_emb # (B, T, C)
+        x = self.sa_heads(x)
+        logits = self.lm_head(x) # (B, T, vocab_size)
+
         if targets is None:
             loss = None
         else:
@@ -75,14 +116,17 @@ class BigramLanguageModel(nn.Module):
     
     def generate(self, idx, max_new_tokens):
         for _ in range(max_new_tokens):
-            logits, loss = self(idx)
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
+            # get thre prediction
+            logits, loss = self(idx_cond)
             logits = logits[:, -1, :]
             probs = F.softmax(logits, 1) # B,C
             idx_next = torch.multinomial(probs, num_samples=1) # B,1
             idx = torch.cat([idx, idx_next], dim=1) # (B, T+1)
         return idx
 
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel()
 m = model.to(device)
 
 optimizer = torch.optim.AdamW(m.parameters(), lr = learning_rate)
