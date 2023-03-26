47afd194247dddfacc64c65720e509f040000e3c
Shriamrut V <vshriamrut@vmware.com>
2023-03-26 11:05:06 +0530

    Transformer completed; drumroll time

--- m-gpt/bigram.py
+++ m-gpt/bigram.py
@@ -3,14 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size  = 32
-block_size = 8
+batch_size  = 64 # How many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length?
 max_iters = 5000
-eval_interval = 300
-learning_rate = 1e-3
+eval_interval = 500
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+n_layer = 6
+n_head = 6
+dropout = 0.2
 #---------------------------------------
 
 print(f"Device {device}")
@@ -58,13 +61,45 @@ def estimate_loss():
     model.train()
     return out
 
+class Block(nn.Module):
+    """Transformer block: communication followed by computation"""
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MuliHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+    
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+    
+class FeedForward(nn.Module):
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd), # projection layer
+            nn.Dropout(dropout)
+        )
+    
+    def forward(self, x):
+        return self.net(x)
+    
 class MuliHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-
+        self.proj = nn.Linear(n_embd, 
+                              n_embd)
+        self.dropout = nn.Dropout(dropout)
     def forward(self, x):
-        return torch.cat([h(x) for h in self.heads], dim = -1)
+        out = torch.cat([h(x) for h in self.heads], dim = -1)
+        out = self.dropout(self.proj(out))
+        return out
        
 class Head(nn.Module):
     def __init__(self, head_size):
@@ -72,6 +107,7 @@ class Head(nn.Module):
         self.key = nn.Linear(n_embd, head_size, bias=False)
         self.query = nn.Linear(n_embd, head_size, bias = False)
         self.value = nn.Linear(n_embd, head_size, bias = False)
+        self.dropout = nn.Dropout(dropout)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
     
     def forward(self, x):
@@ -83,7 +119,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2, -1) * C**0.5 # (B,T,C) @ (B, C, T) = (B, T, T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf")) # (B, T, T)
         wei = F.softmax(wei, dim = -1) # (B, T, T)
-        
+        wei = self.dropout(wei)
         # perform weighted aggregation of values
         v = self.value(x) # (B, T, T)
         out = wei @ v # (B, T, T) @ (B, T, T) = (B, T, T)
@@ -94,7 +130,8 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_heads = MuliHeadAttention(4, n_embd//4) # 4 heads of 8 dimensional self attention
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head = n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
         
     def forward(self, idx, targets = None):
@@ -102,7 +139,8 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T, device = device)) # (T,C)
         x = tok_emb + pos_emb # (B, T, C)
-        x = self.sa_heads(x)
+        x = self.blocks(x) # (B, T, C)
+        x = self.ln_f(x) # (B, T, C)
         logits = self.lm_head(x) # (B, T, vocab_size)
 
         if targets is None:
